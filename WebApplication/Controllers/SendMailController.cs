63218dc0da4806fd5bf69cc58c4791fb263854f7
joeblackradley <joe_collins@blackradley.com>
2014-11-10 14:39:38 +0000

    Environment variables separated out to a constants.cs file.

--- WebApplication/Controllers/SendMailController.cs
+++ WebApplication/Controllers/SendMailController.cs
@@ -6,6 +6,7 @@ using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using SendGrid;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
@@ -23,21 +24,16 @@ namespace WebApplication.Controllers
         {
             if (ModelState.IsValid)
             {
-                String administratorEmail = System.Environment.GetEnvironmentVariable("SENDGRID_DEST");
                 // Create the email object first, then add the properties.
                 var myMessage = new SendGridMessage();
                 myMessage.AddTo(modelMail.From);
-                myMessage.AddTo(administratorEmail);
-                myMessage.From = new MailAddress(administratorEmail, "Insight Website");
+                myMessage.From = new MailAddress(Email.AdministratorAddress, "Insight Website");
                 myMessage.Subject = "Invitation Request";
                 myMessage.Html = modelMail.From;
                 myMessage.EnableTemplateEngine("5ad29324-e439-46ff-b3c6-64d214462920");
 
                 // Create network credentials to access your SendGrid account
-                String username = System.Environment.GetEnvironmentVariable("SENDGRID_USER");
-                String password = System.Environment.GetEnvironmentVariable("SENDGRID_PASS");
-                // Create credentials, specifying your user name and password.
-                var credentials = new NetworkCredential(username, password);
+                var credentials = new NetworkCredential(Email.Username, Email.Password);
                 // Create an Web transport for sending email.
                 var transportWeb = new Web(credentials); 
                 transportWeb.Deliver(myMessage);
---
+++ WebApplication/Models/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public static class Email
+    {
+        static Email()
+        {
+            AdministratorAddress = Environment.GetEnvironmentVariable("SENDGRID_DEST");
+            Username = System.Environment.GetEnvironmentVariable("SENDGRID_USER");
+            Password = System.Environment.GetEnvironmentVariable("SENDGRID_PASS");
+        }
+        public static string AdministratorAddress;
+        public static string Username;
+        public static string Password;
+        public const string ConfirmInvitationTemplate = "5ad29324-e439-46ff-b3c6-64d214462920";
+    }
+}
