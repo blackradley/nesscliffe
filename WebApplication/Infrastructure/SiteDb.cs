382c5d2607c91a018bace218046e99a81b2ef43f
Joe Collins <joejcollins@gmail.com>
2014-11-18 18:48:33 +0000

    Pushed changes to work on laptop.

--- WebApplication/Infrastructure/SiteDb.cs
+++ WebApplication/Infrastructure/SiteDb.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using ClassLibrary;
 
 namespace WebApplication.Infrastructure
 {
-    public class SiteDb : DbContext
+    public class SiteDb : DbContext, ISitesDataSource
     {
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<Month> Months { get; set; }
+
+        IQueryable<Month> ISitesDataSource.Months
+        {
+            get { return Months; }
+        }
+
+        IQueryable<Site> ISitesDataSource.Sites
+        {
+            get { return Sites; }
+        }
     }
 }
