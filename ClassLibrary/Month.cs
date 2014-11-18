dfb78ab5c66c38731509f6635c30ad8333337f9c
Joe Collins <joejcollins@gmail.com>
2014-11-18 13:02:31 +0000

    Data for first page of data entry.

--- ClassLibrary/Month.cs
+++ ClassLibrary/Month.cs
@@ -9,6 +9,19 @@ namespace ClassLibrary
     public class Month
     {
         public virtual Guid Id { get; set; }
-        public virtual DateTime FirstDay { get; set; }
+        public virtual DateTime MonthTime { get; set; }
+        public virtual int MarketingSpend { get; set; }
+        public virtual bool RegionalTv { get; set; }
+        public virtual bool NationalTv { get; set; }
+        public virtual bool OverseasTv { get; set; }
+        public virtual String WebsiteUrl { get; set; }
+        public virtual int WebsiteVisitors { get; set; }
+        public virtual String FacebookUrl { get; set; }
+        public virtual String TwitterUrl { get; set; }
+        public virtual String FlickrUrl { get; set; }
+        public virtual String InstagramUrl { get; set; }
+        public virtual String YoutubeUrl { get; set; }
+        public virtual String VimeoUrl { get; set; }
+        public virtual String PinterestUrl { get; set; }
     }
 }
--- ClassLibrary/Site.cs
+++ ClassLibrary/Site.cs
@@ -12,7 +12,19 @@ namespace ClassLibrary
     public class Site
     {
         public virtual Guid Id { get; set; }
+        public virtual String Owner { get; set; }
+        public virtual String Name { get; set; }
         public virtual String Postcode { get; set; }
+        public virtual bool Museum { get; set; }
+        public virtual bool Castle { get; set; }
+        public virtual bool Gallery { get; set; }
+        public virtual bool WorldHeritageSite { get; set; }
+        public virtual bool HistoricHouse { get; set; }
+        public virtual bool HistoricSite { get; set; }
+        public virtual bool OpenAir { get; set; }
+        public virtual bool Accreditation { get; set; }
+        public virtual int AreaIndoor { get; set; }
+        public virtual int AreaOutdoor { get; set; }
         public virtual ICollection<Month> Months { get; set; }
  
     }
