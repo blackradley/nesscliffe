8d60f5fde42494107891f4343e33f00d361b002e
Joe Collins <joejcollins@gmail.com>
2014-11-18 12:38:42 +0000

    Framework for data access.

---
+++ ClassLibrary/Month.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Month
+    {
+        public virtual Guid Id { get; set; }
+        public virtual DateTime FirstDay { get; set; }
+    }
+}
---
+++ ClassLibrary/Site.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Site
+    {
+        public virtual Guid Id { get; set; }
+        public virtual String Postcode { get; set; }
+        public virtual ICollection<Month> Months { get; set; }
+ 
+    }
+}
