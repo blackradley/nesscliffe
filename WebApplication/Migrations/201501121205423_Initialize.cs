3ef3f6abf5f26fd583f050eb289bc5ddf3d0c0a5
Joe Collins <joejcollins@gmail.com>
2015-01-19 12:46:00 +0000

    Migrations created in testing.

---
+++ WebApplication/Migrations/201501121205423_Initialize.cs
@@ -0,0 +1,198 @@
+namespace WebApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initialize : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MonthArrives",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        HoursMonday = c.Single(nullable: false),
+                        HoursTuesday = c.Single(nullable: false),
+                        HoursWednesday = c.Single(nullable: false),
+                        HoursThursday = c.Single(nullable: false),
+                        HoursFriday = c.Single(nullable: false),
+                        HoursSaturday = c.Single(nullable: false),
+                        HoursSunday = c.Single(nullable: false),
+                        Visitors = c.Int(nullable: false),
+                        IncomeAdmissions = c.Single(nullable: false),
+                        IncomeAdditional = c.Single(nullable: false),
+                        VisitorsAdditional = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Months", t => t.Id)
+                .Index(t => t.Id);
+            
+            CreateTable(
+                "dbo.Months",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        SiteId = c.Guid(nullable: false),
+                        MonthTime = c.DateTime(nullable: false, precision: 7, storeType: "datetime2"),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Sites", t => t.SiteId, cascadeDelete: true)
+                .Index(t => t.SiteId)
+                .Index(t => t.MonthTime, unique: true);
+            
+            CreateTable(
+                "dbo.MonthAttentions",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        MarketingSpend = c.Int(nullable: true),
+                        RegionalTv = c.Boolean(nullable: false),
+                        NationalTv = c.Boolean(nullable: false),
+                        OverseasTv = c.Boolean(nullable: false),
+                        WebsiteUrl = c.String(),
+                        WebsiteVisitors = c.Int(),
+                        FacebookUrl = c.String(),
+                        TwitterUrl = c.String(),
+                        FlickrUrl = c.String(),
+                        InstagramUrl = c.String(),
+                        YoutubeUrl = c.String(),
+                        VimeoUrl = c.String(),
+                        PinterestUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Months", t => t.Id)
+                .Index(t => t.Id);
+            
+            CreateTable(
+                "dbo.Sites",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                        Name = c.String(nullable: false),
+                        Postcode = c.String(nullable: false),
+                        IsMuseum = c.Boolean(nullable: false),
+                        IsAccredited = c.Boolean(nullable: false),
+                        IsCastle = c.Boolean(nullable: false),
+                        IsHistoricHouse = c.Boolean(nullable: false),
+                        IsArtsCentre = c.Boolean(nullable: false),
+                        IsGallery = c.Boolean(nullable: false),
+                        IsWorldHeritageSite = c.Boolean(nullable: false),
+                        IsOpenAir = c.Boolean(nullable: false),
+                        IsHeritageSite = c.Boolean(nullable: false),
+                        IsNationalTrust = c.Boolean(nullable: false),
+                        IsPark = c.Boolean(nullable: false),
+                        IsNatureReserve = c.Boolean(nullable: false),
+                        AreaIndoor = c.Single(nullable: false),
+                        AreaIndoorUnits = c.Int(nullable: false),
+                        AreaOutdoor = c.Single(nullable: false),
+                        AreaOutdoorUnits = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.MonthArrives", "Id", "dbo.Months");
+            DropForeignKey("dbo.Sites", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Months", "SiteId", "dbo.Sites");
+            DropForeignKey("dbo.MonthAttentions", "Id", "dbo.Months");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Sites", new[] { "UserId" });
+            DropIndex("dbo.MonthAttentions", new[] { "Id" });
+            DropIndex("dbo.Months", new[] { "MonthTime" });
+            DropIndex("dbo.Months", new[] { "SiteId" });
+            DropIndex("dbo.MonthArrives", new[] { "Id" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Sites");
+            DropTable("dbo.MonthAttentions");
+            DropTable("dbo.Months");
+            DropTable("dbo.MonthArrives");
+        }
+    }
+}
