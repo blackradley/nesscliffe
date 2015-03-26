48556b9ab7ad1a85941ef231df4fa02addf41a02
Joe Collins <joejcollins@gmail.com>
2015-03-26 21:57:53 +0000

    Added test user for smoke tests.

--- WebApplication/Migrations/Configuration.cs
+++ WebApplication/Migrations/Configuration.cs
@@ -1,3 +1,8 @@
+using DataAccess;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebApplication.Infrastructure;
+
 namespace WebApplication.Migrations
 {
     using System;
@@ -17,20 +22,18 @@ namespace WebApplication.Migrations
             MigrationsDirectory = @"Migrations";
         }
 
-        protected override void Seed(WebApplication.Infrastructure.DataDb context)
+        protected override void Seed(DataDb context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // Add the test user so it is available for smoke tests.
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new ApplicationUserManager(userStore);
+            var user = new ApplicationUser
+            {
+                UserName = "insightblackradley@mailinator.com", 
+                Email = "insightblackradley@mailinator.com",
+                LockoutEnabled = true
+            };
+            userManager.Create(user, "password");
         }
     }
 }
