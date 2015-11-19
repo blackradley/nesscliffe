46566dbcf8bd3ca6cf27666d2502a4cdd84ff9a8
Joe J Collins <joejcollins@gmail.com>
2015-11-19 14:40:46 +0000

    Git hash added to version tag.

---
+++ WebApplication/Helpers/GitCommit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Helpers
+{
+    public static class GitCommit
+    {
+
+        /// <summary>
+        /// When the application is deployed, Kudu writes a file containing the active
+        /// git hash.  For convenience this helper allows it to be pulled into the
+        /// interface.
+        /// </summary>
+        /// <returns></returns>
+        public static string Hash()
+        {
+            string active = "//";
+            if (Environment.GetEnvironmentVariable("HOME") != null)
+            {
+                active = Environment.GetEnvironmentVariable("HOME").ToString() + "//site//deployments//active";
+            }
+            string contents = "In development, no hash file found.";
+            if (System.IO.File.Exists(active))
+            {
+                StreamReader reader = new StreamReader(active);
+                contents = reader.ReadToEnd();
+                reader.Close();
+            }
+            return contents;
+        }
+    }
+}
