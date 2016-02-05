65343865728a62b431088c9620a295a1166ffc6a
Joe J Collins <joejcollins@gmail.com>
2016-02-05 14:39:19 +0000

    Moved the password strength script so it is views specific.  I never liked having it in the Scripts folder.

--- WebApplication/Scripts/_references.js
+++ WebApplication/Scripts/_references.js
@@ -5,6 +5,6 @@
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="modernizr-2.8.3.js" />
-/// <reference path="pwstrength.js" />
 /// <reference path="respond.js" />
+/// <reference path="../views/account/register.js" />
 /// <reference path="../views/sites/report.js" />
